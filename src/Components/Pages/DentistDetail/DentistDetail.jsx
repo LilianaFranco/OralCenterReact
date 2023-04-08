bf0ae914bf288458bd151a833c739adc95593852
LilianaFranco <franco0123@gmail.com>
2023-04-08 15:18:13 -0300

    Create card for a dentist

--- src/Components/Pages/DentistDetail/DentistDetail.jsx
+++ src/Components/Pages/DentistDetail/DentistDetail.jsx
@@ -1,9 +1,74 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Typography,
+} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import PublicIcon from "@mui/icons-material/Public";
 import React from "react";
 
-export const DentistDetail = () => {
+export const DentistDetail = ({ user }) => {
   return (
     <div>
-      <h2>Soy un dentista</h2>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "20px",
+        }}
+      >
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://burst.shopifycdn.com/photos/male-dentist.jpg?width=1200&format=pjpg&exif=1&iptc=1"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Grid item xs={12} md={6}>
+                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                  {user.name}
+                </Typography>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <EmailIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={user.email} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <LocalPhoneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={user.phone} />
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <PublicIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={user.website} />
+                  </ListItem>
+                </List>
+              </Grid>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
     </div>
   );
 };
