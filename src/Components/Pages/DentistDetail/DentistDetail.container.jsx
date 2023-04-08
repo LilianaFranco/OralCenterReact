b3d5b63d7cd156838f035008669bf262b9e704ac
LilianaFranco <franco0123@gmail.com>
2023-04-08 14:25:02 -0300

    Create dentistdetail container

--- src/Components/Pages/DentistDetail/DentistDetail.container.jsx
+++ src/Components/Pages/DentistDetail/DentistDetail.container.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { DentistContext } from "../../../Context/DentistContext.jsx/DentistContext";
 import { DentistDetail } from "./DentistDetail";
 
 export const DentistDetailContainer = () => {
+  const { dispatch } = useContext(DentistContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getUser = axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    getUser.then((res) => dispatch({ type: "GET_USER", payload: res.data }));
+  }, []);
+
   return (
     <div>
       <DentistDetail />
--- src/Components/Pages/DentistDetail/DentistDetail.jsx
+++ src/Components/Pages/DentistDetail/DentistDetail.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Navbar } from "../../Layout/NavBar/Navbar";
 
 export const DentistDetail = () => {
   return (
     <div>
-      <Navbar />
       <h2>Soy un dentista</h2>
     </div>
   );
